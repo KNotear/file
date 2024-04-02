6e985b2d49428519a102dfa1088dae14482cfa7a
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-04-02 15:00:02 +0900

    Update nfs-client.sh

--- aws/nfs-client.sh
+++ aws/nfs-client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum -y install nfs-utils
 mkdir /sharedb
-mount -t nfs 192.168.108.100:/nfs-shared /sharedb
-echo "192.168.108.100:/nfs-shared /test nfs defaults 0 0" >> /etc/fstab
+mount -t nfs xxx.xxx.xxx.xxx:/nfs-shared /sharedb
+echo "xxx.xxx.xxx.xxx:/nfs-shared /test nfs defaults 0 0" >> /etc/fstab
 echo "ec2-metadata -v | cut -d ' ' -f 2" >> /sharedb/list.txt
-umount 192.168.108.100:/nfs-shared
+umount xxx.xxx.xxx.xxx:/nfs-shared
 rmdir /sharedb
