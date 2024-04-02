d535f20af4e1e3bada8ee0b9802d2a42f15220eb
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-04-02 14:50:59 +0900

    Update web-instance.sh

--- aws/web-instance.sh
+++ aws/web-instance.sh
@@ -2,4 +2,4 @@
 yum -y install httpd
 systemctl start httpd
 systemctl enable httpd
-echo "<h1><font color='blue'> 서울 웹 서버 </font></h1>" > /var/www/html/index.html
+echo "<h1><font color='blue'> 웹 서버 </font></h1>" > /var/www/html/index.html
