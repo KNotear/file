fe8d82698971f35c7d28c9446517819c95a47612
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-04-02 15:58:59 +0900

    Update ping.sh

--- aws/ping.sh
+++ aws/ping.sh
@@ -3,8 +3,8 @@ cat /nfs-server/list.txt | while read output
 do
     ping -c 1 -W 1 "$output" > /dev/null
     if [ $? -eq 0 ]; than
-    echo "$output is up"
+    echo "Instance $output is up"
     else
-    echo "$output is down"
+    echo "Instance $output is down"
     fi
 done
