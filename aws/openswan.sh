dcfe948aecb5a7b1a23812462cfe269a90e17ea1
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-04-04 15:22:16 +0900

    Update openswan.sh

--- aws/openswan.sh
+++ aws/openswan.sh
@@ -8,26 +8,32 @@ net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.all.send_redirects = 0
 EOF
 sysctl -p
-cat <<EOF >> /etc/ipsec.d/aws.conf
+printf "Left IP(CGW IP)는 ?"
+read cgwip
+printf "Right IP(TGW IP)는 ?"
+read tgwip
+
+cat <<EOF > /root/aws.conf
 conn Tunnel1
-  authby=secret
-  auto=start
-  left=%defaultroute
-  leftid=52.78.82.59
-  right=13.124.200.153
-  type=tunnel
-  ikelifetime=8h
-  keylife=1h
-  phase2alg=aes128-sha1;modp1024
-  ike=aes128-sha1;modp1024
-  keyingtries=%forever
-  keyexchange=ike
-  leftsubnet=10.2.0.0/16
-  rightsubnet=10.1.0.0/16
-  dpddelay=10
-  dpdtimeout=30
-  dpdaction=restart_by_peer
+	authby=secret
+	auto=start
+	left=%defaultroute
+	leftid='$cgwip'
+	right='$tgwip'
+	type=tunnel
+	ikelifetime=8h
+	keylife=1h
+	phase2alg=aes128-sha1;modp1024
+	ike=aes128-sha1;modp1024
+	keyingtries=%forever
+	keyexchange=ike
+	leftsubnet=10.2.0.0/16
+	rightsubnet=10.1.0.0/16
+	dpddelay=10
+	dpdtimeout=30
+	dpdaction=restart_by_peer
 EOF
-echo "52.78.82.59 13.124.200.153: PSK "password"" > /etc/ipsec.d/aws.secrets
+
+echo "$cgwip $tgwip: PSK "password"" > /etc/ipsec.d/aws.secrets
 ipsec start
 systemctl enable ipsec.service
