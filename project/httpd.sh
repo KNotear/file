48536519a39a7efa569767f34490a70e95f99adb
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-05-30 10:19:34 +0900

    Update httpd.sh

--- project/httpd.sh
+++ project/httpd.sh
@@ -9,7 +9,6 @@ wget https://github.com/KNotear/file/blob/main/project/default.css
 wget https://github.com/KNotear/file/blob/main/project/fonts.css
 availability_zone=$(ec2-metadata -z | cut -d ' ' -f 2)
 cat <<EOF > /var/www/html/index.html
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
