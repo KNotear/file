29e8f8b719c535d656e7b0c7ab4db1820592d39c
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-05-30 15:30:31 +0900

    Update httpd.sh

--- project/httpd.sh
+++ project/httpd.sh
@@ -6,6 +6,7 @@ tar -xvf web.tar
 availability_zone=$(ec2-metadata -z | cut -d ' ' -f 2)
 cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<meta charset="UTF-8">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
