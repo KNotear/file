87b25afe0438e61066f845a4fd4641ac62607028
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-05-30 12:29:12 +0900

    Update tomcat.sh

--- project/tomcat.sh
+++ project/tomcat.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-"yum -y install java",
-"wget http://dlcdn.apache.org/tomcat/tomcat-10/v10.1.24/bin/apache-tomcat-10.1.24.tar.gz",
-"tar xzf apache-tomcat-10.1.24.tar.gz",
-"echo '<%= new java.util.Date()%>' > ./apache-tomcat-10.1.24/webapps/ROOT/test.jsp",
-"echo '[UNIT]' > /usr/lib/systemd/system/tomcat.service",
-"echo 'Description=tomcat' >> /usr/lib/systemd/system/tomcat.service",
-"echo 'After=syslog.target network.target' >> /usr/lib/systemd/system/tomcat.service",
-"echo '[Service]' >> /usr/lib/systemd/system/tomcat.service",
-"echo 'Type=forking' >> /usr/lib/systemd/system/tomcat.service",
-"echo 'ExecStart=/root/apache-tomcat-10.1.24/bin/startup.sh' >> /usr/lib/systemd/system/tomcat.service",
-"echo 'ExecStop=/root/apache-tomcat-10.1.24/bin/shutdown.sh' >> /usr/lib/systemd/system/tomcat.service",
-"echo 'User=root' >> /usr/lib/systemd/system/tomcat.service",
-"echo 'Group=root' >> /usr/lib/systemd/system/tomcat.service",
-"echo '[Install]' >> /usr/lib/systemd/system/tomcat.service",
-"echo 'WantedBy=multi-user.target' >> /usr/lib/systemd/system/tomcat.service",
-"systemctl daemon-reload",
-"systemctl start tomcat",
-"systemctl enable tomcat"
+yum -y install java
+wget http://dlcdn.apache.org/tomcat/tomcat-10/v10.1.24/bin/apache-tomcat-10.1.24.tar.gz
+tar xzf apache-tomcat-10.1.24.tar.gz
+echo '<%= new java.util.Date()%>' > ./apache-tomcat-10.1.24/webapps/ROOT/test.jsp
+echo '[UNIT]' > /usr/lib/systemd/system/tomcat.service
+echo 'Description=tomcat' >> /usr/lib/systemd/system/tomcat.service
+echo 'After=syslog.target network.target' >> /usr/lib/systemd/system/tomcat.service
+echo '[Service]' >> /usr/lib/systemd/system/tomcat.service
+echo 'Type=forking' >> /usr/lib/systemd/system/tomcat.service
+echo 'ExecStart=/root/apache-tomcat-10.1.24/bin/startup.sh' >> /usr/lib/systemd/system/tomcat.service
+echo 'ExecStop=/root/apache-tomcat-10.1.24/bin/shutdown.sh' >> /usr/lib/systemd/system/tomcat.service
+echo 'User=root' >> /usr/lib/systemd/system/tomcat.service
+echo 'Group=root' >> /usr/lib/systemd/system/tomcat.service
+echo '[Install]' >> /usr/lib/systemd/system/tomcat.service
+echo 'WantedBy=multi-user.target' >> /usr/lib/systemd/system/tomcat.service
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
