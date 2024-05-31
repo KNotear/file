df038dcb33ae409aa792b9d5a5b6968521558d99
KNoTear <157345693+KNotear@users.noreply.github.com>
2024-05-31 10:55:30 +0900

    Create proxy_prd.sh

---
+++ project/proxy_prd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cat <<EOF >> /etc/httpd/conf/httpd.conf
+LoadModule proxy_connect_module modules/mod_proxy_connect.so
+LoadModule proxy_module modules/mod_proxy.so
+LoadModule proxy_http_module modules/mod_proxy_http.so
+
+<VirtualHost *:80>
+  ProxyRequests On
+  ProxyPreserveHost On
+  
+  <Proxy *>
+  Order deny,allow
+	Allow from all
+	SetEnv force-proxy-request-1.0.1
+	SetEnv proxy-nokeepalive 1
+	SetEnv proxy-initial-not-pooled 1
+  </Proxy>
+  ProxyPass "/servlet/" "http://internal-PRD-was-alb-662226164.ap-northeast-2.elb.amazonaws.com:8080" ttl=60
+  ProxyPassMAtch "^/.*\.(jsp|do)$" "http://internal-PRD-was-alb-662226164.ap-northeast-2.elb.amazonaws.com:8080/"
+  Timeout 120
+</VirtualHost>
+EOF
+systemctl restart httpd
